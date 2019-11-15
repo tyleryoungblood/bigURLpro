c2e644b60fc0ff219c85edacaa213d0170ab6f67
Tyler Youngblood <tyleryoungblood@yahoo.com>
2019-11-15 15:21:29 -0500

    add logic to switch icon for dark and light mode browsers.

--- background.js
+++ background.js
@@ -4,3 +4,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
    // background.js doesn't.
    chrome.tabs.executeScript(null, {file: "content.js"});
 });
+
+chrome.runtime.onMessage.addListener(function(request) {
+   if ( request.scheme == "dark" )
+      chrome.browserAction.setIcon({
+         path : {
+            "128": "icon128dark.png",
+            "48": "icon48dark.png",
+            "32": "icon32dark.png",
+            "16": "icon16dark.png"
+         }
+      });
+});
